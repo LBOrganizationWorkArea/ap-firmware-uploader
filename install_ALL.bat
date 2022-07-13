0a1e39fb89a9b5d1b20d681e59583f9d3757068a
davide_Lentini <davide.lentini000@gmail.com>
2022-07-13 12:58:21 +0200

    Added bat script to download python installer and install poetry

---
+++ install_ALL.bat
@@ -0,0 +1,5 @@
+bitsadmin /transfer myDownloadJob /download /priority normal https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe %cd%/python_3.10.exe
+python_3.1.0.exe
+pip install poetry
+poetry install
+pause
--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import nmap
 import os
 import sys
+import subprocess
+
+
 firmware=sys.argv[1]
 # send command to cmd
 print("FIRMWARE OTA : " + firmware)
+print("STARTING OTA ! ")
 string = "poetry run ap-uploader " +  " " + (firmware)
 os.system('cmd /k '+string)
-
 
