7cf84fd9e2fb3846dc02e9ece23601b0c2b0877d
davide_Lentini <davide.lentini000@gmail.com>
2022-07-13 14:09:41 +0200

    Added script for install

--- install_ALL.bat
+++ install_ALL.bat
@@ -1,5 +1,5 @@
 bitsadmin /transfer myDownloadJob /download /priority normal https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe %cd%/python_3.10.exe
-python_3.1.0.exe
+python_3.10.exe
 pip install poetry
 poetry install
 pause
